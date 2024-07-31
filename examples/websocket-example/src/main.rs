016e00b32637efd3773150116d93886ea283830c
samjingwen <samjingwen@u.nus.edu>
2024-07-31 21:37:39 +0800

    update websocket example

--- examples/websocket-example/src/main.rs
+++ examples/websocket-example/src/main.rs
@@ -1,11 +1,6 @@
-use futures_util::{pin_mut, SinkExt, StreamExt};
+use tungstenite::{connect, Message};
 
-use tokio_tungstenite::connect_async;
-use tokio_tungstenite::tungstenite::Message;
-use tokio::io::AsyncWriteExt;
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let url = "wss://ws.kraken.com/v2";
     let subscribe_message: &str = r#"
 {
@@ -19,23 +14,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 }
     "#;
 
-    let (ws_stream, _) = connect_async(url).await?;
+    let (mut socket, _) = connect(url)?;
+    println!("Connected to the server");
 
-    let (mut write, read) = ws_stream.split();
-    write.send(Message::text(subscribe_message.to_string())).await?;
+    socket.send(Message::Text(subscribe_message.into()))?;
 
-    let ws_to_stdout = {
-        read.for_each(|message| async {
-            let data = message.unwrap().into_data();
+    loop {
+        let msg = socket.read()?;
+        if let Message::Text(text) = msg {
+            println!("Received: {}", text);
+        }
+    }
 
-            tokio::io::stdout().write_all(&data).await.unwrap();
-            tokio::io::stdout().write_all(b"\n").await.unwrap();
-        })
-    };
-
-    pin_mut!(ws_to_stdout);
-
-    ws_to_stdout.await;
-
-    Ok(())
 }
